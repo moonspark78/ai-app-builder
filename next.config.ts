f70673e16b7eb8295bb0ac51caedf1d12bb08f14
Omar Jabour <jabour.omar@hotmail.com>
2026-06-10 10:15:23 +0200

    :art: init   :art:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
