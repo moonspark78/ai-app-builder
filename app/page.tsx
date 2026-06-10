7331b5ce3d96fc7b972567ef3f98bc478dae10dd
Omar Jabour <jabour.omar@hotmail.com>
2026-06-10 11:21:44 +0200

    :art: init blank page.tsx fix button  .  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant="outline" >Test</Button>
+      <Button variant={"destructive"} >Test</Button>
     </div>
   );
 }
