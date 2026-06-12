ec92be61c0960e3f44429042d03ad0588651a6f1
Omar Jabour <jabour.omar@hotmail.com>
2026-06-12 18:19:09 +0200

    :art: working on the Header.tsx  :art:

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,6 +27,7 @@ const Header = () => {
 
           <span className="inline-flex h-8 items-center gap-1.5 rounded-full border border-white/10 bg-white/5 px-3 text-xs text-white/70">
             <Zap className="h-3 w-3 fill-white/70"/>
+            3 / 40 credits
           </span>
         </div>
       </nav>
