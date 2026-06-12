445582d74343120303bbe666f7d6b3fffa47ac10
Omar Jabour <jabour.omar@hotmail.com>
2026-06-12 17:42:16 +0200

    :art: working on the Header.tsx  /  :art:

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-50 h-16 border-b border-white/6 bg-white/7 backdrop-blur-md">
       <nav className="mx-auto flex h-full max-w-7xl items-center justify-between px-4">
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/">
           <Image
             src={"/logo.png"}
             alt="Logo"
