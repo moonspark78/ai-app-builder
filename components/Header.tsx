3eb1411b96f0c1499bf59d067b808940cab263c2
Omar Jabour <jabour.omar@hotmail.com>
2026-06-12 18:08:58 +0200

    :art: working on the Header.tsx  /././.  :art:

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
-import Link from 'next/link'
+import { Zap } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 z-50 h-16 border-b border-white/6 bg-white/7 backdrop-blur-md">
+    <header className="w-full  fixed top-0 left-0 z-50 h-16 border-b border-white/6 bg-white/7 backdrop-blur-md">
       <nav className="mx-auto flex h-full max-w-7xl items-center justify-between px-4">
         <Link href="/">
           <Image
@@ -23,6 +24,10 @@ const Header = () => {
           >
             Projects
           </Link>
+
+          <span>
+            <Zap className="h-3 w-3 fill-white/70"/>
+          </span>
         </div>
       </nav>
     </header>
