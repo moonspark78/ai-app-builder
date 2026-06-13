0f1ca80f260bc09efb2354eec6f1d48fd68daea6
Omar Jabour <jabour.omar@hotmail.com>
2026-06-13 13:09:37 +0200

    :art: create proxy.ts  /  :art:

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { Zap } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Button } from './ui/button'
 
 
 const Header = () => {
@@ -37,7 +38,9 @@ const Header = () => {
 
 
           <Show when="signed-out">
-              <SignInButton mode="modal"/>
+              <SignInButton mode="modal">
+                <Button>Sign in</Button>
+              </SignInButton>
               <SignUpButton mode="modal">
                 <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
