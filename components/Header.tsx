56504a154f1487cd370cd1e20bc690a8b2a877e2
Omar Jabour <jabour.omar@hotmail.com>
2026-06-13 13:04:58 +0200

    :art: create proxy.ts  ././  :art:

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
 
           <Show when="signed-out">
               <SignInButton mode="modal"/>
-              <SignUpButton>
+              <SignUpButton mode="modal">
                 <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>
