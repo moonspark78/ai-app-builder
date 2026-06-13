95ec21063be8eee2253e40b66807e980ec00044d
Omar Jabour <jabour.omar@hotmail.com>
2026-06-13 13:02:05 +0200

    :art: create proxy.ts .  :art:

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { Show, UserButton } from '@clerk/nextjs'
+import { Show, SignUpButton, UserButton } from '@clerk/nextjs'
 import { Zap } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
