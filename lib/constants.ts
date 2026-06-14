d9639c42411f24c1bde14b9e27684a5cda17c8a1
Omar Jabour <jabour.omar@hotmail.com>
2026-06-15 00:13:32 +0200

    :art: Working on the constant.ts :art:

---
+++ lib/constants.ts
@@ -0,0 +1,17 @@
+export const PLANS = {
+  free: {
+    label: "Free",
+    credits: 10,
+    price: 0,
+  },
+  starter: {
+    label: "Starter",
+    credits: 50,
+    price: 9,
+  },
+  pro: {
+    label: "Pro",
+    credits: 150,
+    price: 29,
+  },
+} as const;
