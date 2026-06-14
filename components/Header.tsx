a25635075ac97bb47a85cf1573483b7a762fd3e0
Omar Jabour <jabour.omar@hotmail.com>
2026-06-14 12:58:03 +0200

    :art: Working on the Header.tsx /  :art:

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,9 +44,9 @@ const Header = () => {
                 </Button>
               </SignInButton>
               <SignUpButton mode="modal">
-                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
+               <Button size="sm" className={"text-white/40"}>
+                  Sign up
+                </Button>
               </SignUpButton>
             </Show>
             
