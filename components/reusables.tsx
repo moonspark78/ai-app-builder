0a35c0efe507eaf6909e575a234f4830ff300fa6
Omar Jabour <jabour.omar@hotmail.com>
2026-06-16 19:27:15 +0200

    :art: Working on the Page.tsx /./.  :art:

--- components/reusables.tsx
+++ components/reusables.tsx
@@ -1,3 +1,5 @@
 import React from "react";
 
-export const GrayTitle = ({children} : {children: React.ReactNode}) => {}
+export const GrayTitle = ({children} : {children: React.ReactNode}) => {
+    return <span className="text-white/90">{children}</span>
+}
