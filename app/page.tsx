38bee44fa20bc4f849403f00c016cdb6b8902593
Omar Jabour <jabour.omar@hotmail.com>
2026-06-16 18:53:15 +0200

    :art: Working on the Header.tsx  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { StarsBackground } from "../components/animate-ui/components/backgrounds
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#0a0a0a] selection:bg-white/20">
-      <section className="relative flex flex-col items-center overflow-hidden px-4 pb-24 pt-40 text-center">
+      <section className="relative h-screen flex flex-col items-center overflow-hidden px-4 pb-24 pt-40 text-center">
         <StarsBackground  className="absolute inset-0 h-full w-full"/>
         <Badge variant={"outline"} className="gap-2 p-4 backdrop-blur-sm">
             <div className="h-1.5 w-1.5 animate-pulse rounded-full bg-emerald-400"/>
