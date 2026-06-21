800bdcd3f6314955cd5089f1484f704f086f3568
Omar Jabour <jabour.omar@hotmail.com>
2026-06-21 10:29:25 +0200

    :art: Étape : Page.tsx  ./././././  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { StarsBackground } from "../components/animate-ui/components/backgrounds/stars";
 import { BlueTitle, GrayTitle } from "../components/reusables";
