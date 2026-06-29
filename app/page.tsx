f1ddfdccf1b6e33f1c0079d819f3c6eb71d01fee
Omar Jabour <jabour.omar@hotmail.com>
2026-06-29 19:47:32 +0200

    :art: working on Page.tsx   /././.  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -124,6 +124,7 @@ export default function Home() {
               <button
                 key={s}
                 // onClick={() => handleSuggestion(s)}
+                className="rounded-full"
               >
                 {s}
               </button>
