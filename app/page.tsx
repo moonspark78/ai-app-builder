42452b52c1ffddb171bc0fd8d1b49771ac496fef
Omar Jabour <jabour.omar@hotmail.com>
2026-06-29 19:44:56 +0200

    :art: working on Page.tsx   /.././././/  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
           <div>
             {SUGGESTIONS.map((s) => (
               <button>
-                {}
+                {s}
               </button>
             ))}
           </div>
