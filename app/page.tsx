59098445db7aee3a7139e3aeacfb623a7d06dac5
Omar Jabour <jabour.omar@hotmail.com>
2026-06-29 19:45:30 +0200

    :art: working on Page.tsx   /.././././  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -119,7 +119,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div>
+          <div className="mt-4 flex flex-wrap">
             {SUGGESTIONS.map((s) => (
               <button>
                 {s}
