ab77088d1518f2fb4d7bca771f6ae3207205de51
Omar Jabour <jabour.omar@hotmail.com>
2026-07-05 01:18:44 +0200

    :art: working on Page.tsx  /./.  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="relative mx-auto mb-3é max-w-5xl overflow-hidden rounded-2xl border border-white/8 px-10">
+      <section className="relative mx-auto mb-3é max-w-5xl overflow-hidden rounded-2xl border border-white/8 px-10 py-24 text-center">
         
       </section>
     </main>
