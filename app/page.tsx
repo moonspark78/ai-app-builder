229fa0bd4020a014b0c042a05ba259aa8e6c43d2
Omar Jabour <jabour.omar@hotmail.com>
2026-07-04 12:53:30 +0200

    :art: working on Page.tsx  /././././  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
           {STEPS.map((step, i) => (
             <div key={step.number} className="flex gap-6 ">
               <div className="flex flex-col items-center">
-                <div>
+                <div className="flex h-10 w-10 items-center">
                   <span className="font-mono text-xs font-semibold text-white/50">
                     {step.number}
                   </span>
